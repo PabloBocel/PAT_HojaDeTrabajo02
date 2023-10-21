0d08060e2a64b488ee6055f52c65db072b6f0bbc
Pablo <andresbocel0@gmail.com>
2023-10-21 12:38:20 -0600

    Ejercicio4resuelto

--- PAT_HojaDeTrabajo02/Ejercicio04.h
+++ PAT_HojaDeTrabajo02/Ejercicio04.h
@@ -1,19 +1,16 @@
 #pragma once
-class FrontMiddleBackQueue
-{
+#include <list>
+
+class FrontMiddleBackQueue {
 public:
-	FrontMiddleBackQueue();
-
-	void pushFront(int value);
-
-	void pushMiddle(int value);
-
-	void pushBack(int value);
-
-	int popFront();
-
-	int popMiddle();
-
-	int popBack();
+    FrontMiddleBackQueue();
+    void pushFront(int value);
+    void pushMiddle(int value);
+    void pushBack(int value);
+    int popFront();
+    int popMiddle();
+    int popBack();
+private:
+    std::list<int> data;
 };
 
